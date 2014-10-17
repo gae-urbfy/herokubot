7b2bcc09142f51e3dfe636743cc566d1047ff035
Prashanth C A <prashanthca@prashanth-PC>
2014-10-18 01:00:36 +0530

    ASDASD

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-  echo file_get_contents("http://two.hackapis.cf");
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HerokuBot</title>
+	<link rel="stylesheet" type="text/css" href="https://d4yt8xl9b7in.cloudfront.net/assets/application-cf87f0cc4846443bf758750e2cc27ed6.css"/>
+	<style type="text/css">
+		body {
+			font-family: BentonSans;
+			background: #f4f4f4;
+		}
+	</style>
+</head>
+<body>
+	<div id="header">
+		<h2 id="title">HerokuBot</h2>
+		<h4 id="subtitle">Keep your heroku apps alive, always</h4>
+	</div>
+</body>
+</html>
