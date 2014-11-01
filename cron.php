aed1c9bddc9502167a7425670b3390dc8e2c8d45
Prashanth C A <prashanthca@prashanth-PC>
2014-11-02 00:22:21 +0530

    ASDAS

--- cron.php
+++ cron.php
@@ -1,5 +1,4 @@
 <?php
-	date_default_timezone_set("Asia/Kolkata");
-    $result = file_get_contents('http://requestb.in/1clfhho1?time='.date('H:i:s A'));
+    $result = file_get_contents("http://requestb.in/1clfhho1?time=".urlencode(date('H:i:s A')));
     echo $result;
 ?>
